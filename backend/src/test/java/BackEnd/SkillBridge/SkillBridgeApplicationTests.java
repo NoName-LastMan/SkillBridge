21add50b36cef565dc514d530b4e216e76763e27
NoName-LastMan <humamunnajah@gmail.com>
2026-07-02 21:58:49 +0700

    SetUp github structur repo React and Spirng Boot

---
+++ backend/src/test/java/BackEnd/SkillBridge/SkillBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package BackEnd.SkillBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
