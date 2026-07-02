cea32af0fe3445af6b5c0b518a7ef5161da1e5cf
muhamadsalman623 <muhamadsalman623@gmail.com>
2026-07-02 22:44:36 +0700

    ini coba coba yaa

--- backend/src/main/java/BackEnd/SkillBridge/SkillBridgeApplication.java
+++ backend/src/main/java/BackEnd/SkillBridge/SkillBridgeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkillBridgeApplication {
-
+salman
 	public static void main(String[] args) {
 		SpringApplication.run(SkillBridgeApplication.class, args);
 	}
