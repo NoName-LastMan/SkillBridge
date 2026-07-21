efcd5628a6794a199a205095a72f8beb7e6f45bd
muhamadsalman623 <muhamadsalman623@gmail.com>
2026-07-21 15:58:09 +0700

    salman push

--- backend/src/main/java/BackEnd/SkillBridge/SkillBridgeApplication.java
+++ backend/src/main/java/BackEnd/SkillBridge/SkillBridgeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkillBridgeApplication {
-salman
 	public static void main(String[] args) {
 		SpringApplication.run(SkillBridgeApplication.class, args);
 	}
